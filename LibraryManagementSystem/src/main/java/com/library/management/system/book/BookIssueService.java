f8ed2d163faa378062c8d42721283c8a6f9d4557
akashgond3112 <akashgond3112@gmail.com>
2025-01-14 17:24:22 +0000

    commit : 14/01/2025

---
+++ LibraryManagementSystem/src/main/java/com/library/management/system/book/BookIssueService.java
@@ -0,0 +1,33 @@
+package com.library.management.system.book;
+
+
+import com.library.management.system.FineService;
+import com.library.management.system.SystemUser;
+
+class BookIssueService {
+	FineService fineService;
+
+	public BookReservationDetails getReservationDetails(Book book) {
+		return null;
+	}
+
+	public void updateReservationDetails(Book book) {
+
+	}
+
+	public BookIssueDetails reserveBook(Book book, SystemUser user) {
+		return null;
+	}
+
+	public BookIssueDetails issueBook(Book book, SystemUser user) {
+		return null;
+	}
+
+	public BookIssueDetails renewBook(Book book, SystemUser user) {
+		return null;
+	}
+
+	public void returnBook(Book book, SystemUser user) {
+
+	}
+}
