13b7cd7f572cb389dff26bcb0ac6b77f8ebde600
akashgond3112 <akashgond3112@gmail.com>
2025-01-27 17:58:43 +0000

    commit : 27/01/2025

---
+++ ParkingLot/src/main/java/com/java/design/patterns/domain/Parking/Address.java
@@ -0,0 +1,5 @@
+package com.java.design.patterns.domain.Parking;
+
+public class Address {
+    String streetName;
+}
---
+++ ParkingLot/src/main/java/com/java/design/patterns/domain/Parking/ParkingSize.java
@@ -0,0 +1,7 @@
+package com.java.design.patterns.domain.Parking;
+
+public enum ParkingSize {
+    SMALL,
+    MEDIUM,
+    LARGE;
+}
