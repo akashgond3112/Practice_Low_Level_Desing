bf7c377f938ced7d7f6a865a2a8ff0f5e2805377
akashgond3112 <akashgond3112@gmail.com>
2025-01-29 17:16:32 +0000

    commit : 29/01/2025

---
+++ ParkingLot/src/main/java/com/java/design/patterns/domain/Vehicle/Scooter.java
@@ -0,0 +1,9 @@
+package com.java.design.patterns.domain.Vehicle;
+
+import static ParkingLot.domain.Vehicle.VehicleSize.TWO_VEHICLER;
+
+public class Scooter extends Vehicle {
+    public Scooter(String plateNumber, VehicleType vehicleType) {
+        super(TWO_VEHICLER, plateNumber, vehicleType);
+    }
+}
---
+++ ParkingLot/src/main/java/com/java/design/patterns/domain/Vehicle/Truck.java
@@ -0,0 +1,10 @@
+package com.java.design.patterns.domain.Vehicle;
+
+import static ParkingLot.domain.Vehicle.VehicleSize.FOUR_VEHICLER_LARGE;
+
+public class Truck extends Vehicle {
+
+    public Truck(VehicleSize vehicleSize, String plateNumber, VehicleType vehicleType) {
+        super(FOUR_VEHICLER_LARGE, plateNumber, vehicleType);
+    }
+}
