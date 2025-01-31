9f5be77cee216dbf38b62ac370d2739b3aa4d944
akashgond3112 <akashgond3112@gmail.com>
2025-01-31 18:38:44 +0000

    commit : 31/01/2025

---
+++ PropertyHuntApplication/src/main/java/com/java/design/patterns/enums/ListingType.java
@@ -0,0 +1,5 @@
+package com.java.design.patterns.enums;
+
+public enum ListingType {
+	SELL, RENT
+}
---
+++ PropertyHuntApplication/src/main/java/com/java/design/patterns/enums/RoomType.java
@@ -0,0 +1,5 @@
+package com.java.design.patterns.enums;
+
+public enum RoomType {
+	ONEBHK, TWOBHK, THREEBHK
+}
