5be0f29808e4b002aba08f75224c930b4a7805e6
akashgond3112 <akashgond3112@gmail.com>
2025-02-14 19:10:33 +0000

    commit : 14/02/2025

---
+++ RideSharingApplication/src/main/java/com/java/design/patterns/service/IRideService.java
@@ -0,0 +1,10 @@
+package com.java.design.patterns.service;
+
+
+import com.java.design.patterns.model.Ride;
+
+public interface IRideService {
+    public Ride offer_ride(String user, String source, String destination, int availableseats, String vehicle, String vehicleNumber);
+    public Ride select_ride(String user, String source, String destination, int seats, String strategy);
+    public void end_ride(Ride ride);
+}
