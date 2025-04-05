f8beb99cbe27a6e13372ea7fa4f0d6515af2ae73
akashgond3112 <akashgond3112@gmail.com>
2025-04-05 19:01:31 +0100

    commit : 05/04/2025

---
+++ Splitwise/src/main/java/com/java/lld/design/model/split/EqualSplit.java
@@ -0,0 +1,10 @@
+package com.java.lld.design.model.split;
+
+
+import com.java.lld.design.model.User;
+
+public class EqualSplit extends Split {
+    public EqualSplit(User user) {
+        super(user);
+    }
+}
