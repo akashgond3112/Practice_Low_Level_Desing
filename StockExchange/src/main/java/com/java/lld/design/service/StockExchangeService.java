76ceef96146fc2ceefcf40f73faf9b8821469d19
akashgond3112 <akashgond3112@gmail.com>
2025-04-08 20:02:41 +0100

    commit : 08/04/2025

---
+++ StockExchange/src/main/java/com/java/lld/design/service/StockExchangeService.java
@@ -0,0 +1,13 @@
+package com.java.lld.design.service;
+
+import java.time.LocalTime;
+
+/*
+The service which will be implemented by the clients
+ */
+public interface StockExchangeService {
+
+    void addSellOrder(String orderId, String stockName, Double price, int quantity, LocalTime time);
+
+    void matchAndExecuteBuyOrder(String orderId, String stockName, Double price, int quantity, LocalTime time);
+}
